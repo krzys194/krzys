350c44f7d118e281bd4c7e577291ff714a4c6c22
krzys194 <61507827+krzys194@users.noreply.github.com>
2020-04-24 13:10:55 +0200

    Add files via upload

---
+++ Projekt4/Zadanie2.java
@@ -0,0 +1,24 @@
+package Projekt4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (;;) {
+            System.out.println("\nPodaj liczbe:");
+            String s = scanner.nextLine();
+            try {
+                int i = Integer.parseInt(s);
+                System.out.println(" String ---> Int : " + i);
+            } catch (NumberFormatException e) {
+                System.out.println(" Zły format! (" + s + ") nie jest liczbą!");
+            }
+        }
+
+    }
+
+}
+
+

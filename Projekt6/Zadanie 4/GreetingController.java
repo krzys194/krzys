8197686ca5b037d141b303516cac83559af14131
krzys194 <61507827+krzys194@users.noreply.github.com>
2020-06-05 22:46:25 +0200

    Add files via upload

---
+++ Projekt6/Zadanie 4/GreetingController.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class GreetingController {
+    private static final String template = " Hello, %s!, name %s!,age %s!";
+
+
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "age", defaultValue = "0") String age, @RequestParam(value = "id", defaultValue = "id") String id) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, age));
+    }
+
+
+}
